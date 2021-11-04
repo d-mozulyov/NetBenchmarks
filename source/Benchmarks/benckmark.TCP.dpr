b7325b9a8bcb505d6c5f4a7e89a732d55b23df6a
d.mozulyov <softforyou@inbox.ru>
2021-11-04 21:29:58 +0300

    Небольшое развитие бенчмарка, якобы из Net.CrossSocket. Но вообще надо переделать под IOCP

--- source/Benchmarks/benckmark.TCP.dpr
+++ source/Benchmarks/benckmark.TCP.dpr
@@ -4,16 +4,44 @@ program benckmark.TCP;
 
 uses
   uBenchmarks,
-  System.SysUtils;
+  System.SysUtils,
+  Net.CrossSocket.Base,
+  Net.CrossSocket;
 
 type
   TTCPClient = class(TBenchmarkClient)
   protected
+    FConnection: ICrossConnection;
 
+    class procedure BenchmarkInitBlank; override;
+    class procedure BenchmarkInitWork; override;
   public
+    procedure Init; override;
+    procedure Stop; override;
 
+    property Connection: ICrossConnection read FConnection;
   end;
 
+
+{ TTCPClient }
+
+class procedure TTCPClient.BenchmarkInitBlank;
+begin
+end;
+
+class procedure TTCPClient.BenchmarkInitWork;
+begin
+end;
+
+procedure TTCPClient.Init;
+begin
+ // FConnection := TCrossConnection.Create(nil, );
+end;
+
+procedure TTCPClient.Stop;
+begin
+  FConnection := nil;
+end;
 
 begin
   TBenchmark.Run(TTCPClient, [
