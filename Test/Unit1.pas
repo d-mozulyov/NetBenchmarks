6b49de2341fb612d713a199079c47fbbc521bc0a
d.mozulyov <softforyou@inbox.ru>
2021-12-15 12:30:00 +0300

    Развитие

--- Test/Unit1.pas
+++ Test/Unit1.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,
-  IdGlobal, IdContext, IdTCPClient, IdTCPServer;
+  IdGlobal, IdContext, IdTCPClient, IdTCPServer, Winapi.Winsock2;
 
 type
   TForm1 = class(TForm)
@@ -21,6 +21,8 @@ type
   private
     FTCPServer: TIdTCPServer;
     FTCPClient: TIdTCPClient;
+    FIOCPHandle: THandle;
+    FSocketHandle: Winapi.Winsock2.TSocket;
 
     procedure DoServerExecute(AContext: TIdContext);
     function PackMessage: TIdBytes;
@@ -29,9 +31,11 @@ type
   end;
 
 const
+  SERVER_HOST: string = '127.0.0.1';
   SERVER_PORT = 4321;
 
 var
+  WsaData: TWsaData;
   Form1: TForm1;
 
 implementation
@@ -39,6 +43,11 @@ implementation
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  LSockAddr: TSockAddrIn;
+  LAsciiStr: string;
+  LAnsiStr: AnsiString;
+  LHostEnt: PHostEnt;
 begin
   FTCPServer := TIdTCPServer.Create(nil);
   FTCPServer.OnExecute := DoServerExecute;
@@ -46,13 +55,38 @@ begin
   FTCPServer.Active := True;
 
   FTCPClient := TIdTCPClient.Create(nil);
-  FTCPClient.Connect('127.0.0.1', SERVER_PORT);
+  FTCPClient.Connect(SERVER_HOST, SERVER_PORT);
 
+  FIOCPHandle := CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, System.CPUCount);
+  if (FIOCPHandle = 0) then
+    RaiseLastOSError;
+
+  FillChar(LSockAddr, SizeOf(LSockAddr), #0);
+  LSockAddr.sin_family := AF_INET;
+  LSockAddr.sin_port := htons(SERVER_PORT);
+  LHostEnt := nil;
+  SetLength(LAsciiStr, IdnToAscii(0, PChar(SERVER_HOST), Length(SERVER_HOST), nil, 0));
+  if Length(LAsciiStr) > 0 then
+  begin
+    SetLength(LAsciiStr, IdnToAscii(0, PChar(SERVER_HOST), Length(SERVER_HOST), PChar(LAsciiStr), Length(LAsciiStr)));
+    LAnsiStr := AnsiString(LAsciiStr);
+    LHostEnt := gethostbyname(Pointer(LAnsiStr));
+  end;
+  if Assigned(LHostEnt) then
+    LSockAddr.sin_addr.s_addr := PCardinal(LHostEnt.h_addr_list^)^;
+
+  FSocketHandle := WSASocket(PF_INET, SOCK_STREAM, IPPROTO_TCP, nil, 0, WSA_FLAG_OVERLAPPED);
+  if (FSocketHandle= INVALID_SOCKET) then
+    RaiseLastOSError;
+  if (CreateIoCompletionPort(FSocketHandle, FIOCPHandle, 0, System.CPUCount) = 0) then
+    RaiseLastOSError;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
-
+  shutdown(FSocketHandle, SD_BOTH);
+  closesocket(FSocketHandle);
+  CloseHandle(FIOCPHandle);
   FTCPClient.Free;
   FTCPServer.Free;
 end;
@@ -108,5 +142,12 @@ procedure TForm1.btnSendViaIOCPClick(Sender: TObject);
 begin
 //
 end;
+
+
+initialization
+  WSAStartup(WINSOCK_VERSION, WsaData);
+
+finalization
+  WSACleanup;
 
 end.
